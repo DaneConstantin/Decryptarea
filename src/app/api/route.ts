c3230d5cc8c46c86684d10b3caefafcb6aa17359
DanCoDesign <dancodesign@proton.me>
2024-10-10 14:35:15 +0300

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,11 +4,11 @@ import { ObjectId } from 'mongodb';
 export async function GET() {
     try {
         const client = await connect;
-        const cursor = await client.db("CuratedNews").collection("News").find();
+        const cursor = client.db("CuratedNews").collection("News").find();
         const newsArticles = await cursor.toArray();
         return new Response(JSON.stringify(newsArticles), { status: 200 });
     } catch (error) {
-        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+        return new Response(JSON.stringify({ error: error }), { status: 500 });
     }
 }
 
@@ -29,7 +29,7 @@ export async function POST(request: Request) {
 
         return new Response(JSON.stringify({ message: "Article added successfully", result }), { status: 201 });
     } catch (error) {
-        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+        return new Response(JSON.stringify({ error: error }), { status: 500 });
     }
 }
 
@@ -47,6 +47,6 @@ export async function PUT(request: Request) {
 
         return new Response(JSON.stringify({ message: "Successfully updated the document", result }), { status: 200 });
     } catch (error) {
-        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+        return new Response(JSON.stringify({ error: error }), { status: 500 });
     }
 }
