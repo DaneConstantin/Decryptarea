347600b54ca3376d2c2710f8911adb4986c44af3
DanCoDesign <dancodesign@proton.me>
2024-10-24 11:26:46 +0300

    try 2

--- src/app/(main)/guides/[slug]/page.tsx
+++ src/app/(main)/guides/[slug]/page.tsx
@@ -18,7 +18,7 @@ export async function generateStaticParams() {
     return posts.map((post) => post.slug.current)
 }
 
-export default async function Page({ params }: QueryParams) {
+export default async function Page({ params }: { params: QueryParams }) {
     const { slug } = params; // destructure the slug property
     const post = await sanityFetch<SanityDocument>({ query: GUIDE_QUERY, params: { slug } }) // pass the slug property to sanityFetch
     if (!post) {
